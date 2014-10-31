f0d91c9c1e00578da226703f85c424530e79dfdc
Doan Truong Thi <t@thi.codes>
2014-10-31 20:45:28 +0900

    Add launch screen and sign in view controller

--- TextEthan/Controllers/ViewController.swift
+++ TextEthan/Controllers/ViewController.swift
@@ -2,18 +2,41 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  @IBOutlet var firstNameField: UITextField!
+  @IBOutlet var lastNameField: UITextField!
+  @IBOutlet var signInButton: UIButton!
+  @IBOutlet var signInView: UIView!
+  @IBOutlet var infoView: UIView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    if infoView != nil {
+      infoView.hidden = true
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("signedIn") != nil {
+      infoView.hidden = false
+      signInView.hidden = true
+      signInButton.hidden = true
+
+      SupportKit.show()
+    }
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  override func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return UIStatusBarStyle.LightContent
   }
   
+  @IBAction func nameChanged(sender: AnyObject) {
+  }
   
+  @IBAction func signIn(sender: AnyObject) {
+  }
+
+  @IBAction func showMessages(sender: AnyObject) {
+  }
+
   /*
   // MARK: - Navigation
   
