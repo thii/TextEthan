07116c10a760d4b988b44435b2e816406717bcf4
Doan Truong Thi <t@thi.codes>
2014-10-31 19:44:38 +0900

    Add SupportKit app settings

--- TextEthan/Controllers/AppDelegate.swift
+++ TextEthan/Controllers/AppDelegate.swift
@@ -3,4 +3,18 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+    
+    let appTokenData = NSDictionary(contentsOfFile: NSBundle.mainBundle().pathForResource("AppToken", ofType: "plist")!)
+    let appToken: AnyObject? = appTokenData?.objectForKey("AppToken")
+    
+    var settings = SKTSettings(appToken: appToken as String)
+    settings.enableAppWideGesture = false
+    settings.enableGestureHintOnFirstLaunch = false
+    
+    SupportKit.initWithSettings(settings)
+
+    return true
+  }
 }
